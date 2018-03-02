558f794e8f7e56748f205e42db2afbb8ec1fa9a8
Frederic Medery <fmedery@gmail.com>
2018-03-02 16:26:03 -0500

    aws part is donegit

--- 00_definition.tf
+++ 00_definition.tf
@@ -17,35 +17,3 @@ variable "resource_group_name" {
 }
 
 variable "nbr" {}
-
-/*
-## command for demo
-watch awless --aws-profile=localdev --aws-region=us-west-2  list instances --filter name=demo,state=running --sort name
-
-## QA
-terraform plan -var 'nbr=2' -var 'env=qa' -var 'instance=t2.large' -state qa/terraform.tfstate
-terraform apply -var 'nbr=2' -var 'env=qa' -var 'instance=t2.large' -state qa/terraform.tfstate
-
-terraform plan -var 'nbr=10' -var 'env=qa' -var 'instance=t2.large' -state qa/terraform.tfstate
-terraform apply -var 'nbr=10' -var 'env=qa' -var 'instance=t2.large' -state qa/terraform.tfstate
-
-
-terraform destroy  -var 'nbr=2' -var 'env=qa' -var 'instance=t2.large'  -state qa/terraform.tfstate
-
-
-## PROD
-
-
-<<<<<<< HEAD:localdev/devops/demo-fmedery/00_definition.tf
-terraform plan -var 'nbr=10' -var 'env=prod' -var 'instance=t2.large' -state prod/terraform.tfstate
-terraform apply -var 'nbr=10' -var 'env=prod' -var 'instance=t2.large' -state prod/terraform.tfstate
-=======
-terraform apply -var 'nbr=10' -var 'instance=t2.large'
->>>>>>> 93bc5869c8ba9c06b7e5b0371054524be1ea967b:localdev/devops/demo-fmedery/qa/00_definition.tf
-
-
-terraform destroy -state prod/terraform.tfstate
-
-
-*/
-
